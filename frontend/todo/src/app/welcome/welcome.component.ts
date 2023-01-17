275fd8d7a25239b53d8df8c1e4d284998e41089b
Zhuoda <lzd020815@gmail.com>
2023-01-17 11:07:47 -0600

    test5

---
+++ frontend/todo/src/app/welcome/welcome.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css']
+})
+
+export class WelcomeComponent implements OnInit {
+
+  message = 'Some self-defined message'
+  name = ''
+  //Activate route
+  constructor(private route : ActivatedRoute){ }
+
+  ngOnInit(): void {
+      this.name = this.route.snapshot.params['name']
+      console.log(this.message)
+      // console.log(this.route.snapshot.params['name'])
+  }
+}
